a4f9d00844dca4b0b6af800d4a795513aefa1196
VictoriaUsman <iantrisdc@gmail.com>
2025-10-10 21:11:53 +0800

    Add files via upload
    FIrst ever code..

---
+++ BasketBall TeamName Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the best Basketball Team Name Generator!\nLet's Make this teamto the Championship Baby!")
+Name1 = input("Where do you live currently?")
+Name2 = input("What is the best food for you?")
+
+print(Name1 + " " + Name2 + " is the Team going to the Finals, Baby!!"
+                            "")
+
