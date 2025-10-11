55dd8a59f1422c21daceb754a6eb334a38c06fa3
VictoriaUsman <iantrisdc@gmail.com>
2025-10-11 16:39:02 +0800

    Add files via upload

---
+++ OddEven Number Identifier.py
@@ -0,0 +1,8 @@
+
+print("Welcome to Odd/Even Number Identifier")
+Number = (float(input("Please enter the number: ")))
+
+if Number % 2 == 0:
+    print("Even!")
+if Number % 2 != 0:
+    print("That's Odd!")
