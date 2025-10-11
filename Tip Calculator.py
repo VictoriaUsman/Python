692040b4c96cbe1ada9d9d4cd7d862f47750d807
VictoriaUsman <iantrisdc@gmail.com>
2025-10-11 11:29:26 +0800

    Add files via upload
    Tip Calculator V2

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -4,4 +4,4 @@ tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
 Tip_for_each_Person = (bill/people) * tip/100
-print(f"Each person should pay: ${Tip_for_each_Person}")
+print(f"Each person should pay: ${round(Tip_for_each_Person,2)}")
